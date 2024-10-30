b87f3a4e3b6f12ae686387a0109e1c77393539b4
sofguare <pablo.jusim@gmail.com>
2024-10-30 09:28:11 -0300

    '''Subida de archivos provisorios de front end y limpieza de la base de datos'''

---
+++ Front end Proyecto San Jorge.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 14:52:27 2024
+
+@author: Pablo
+
+Front end para la visualizacion de graficos realizado para el area de RRHH
+de la Clinica San Jorge (Ushuaia)
+"""
+# %% Importaciones
+import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from collections import Counter
+
+# %% Graficos institucionales
+
+color_fondo = 'RoyalBlue4'  # Azul similar al de la clinica
+logo = 'Logo_clinica_san_jorge_chico.png'
+
+
+
+# %% Ventana principal
+
+class VentanaPrincipal(tk.Tk):
+    def __init__(self,):
+        super().__init__()
+        self.state('zoomed')  # ventana maximizada
+        self.title('Análisis de ausencias Clínica San Jorge')
+        self['bg'] = color_fondo
+        try:
+            self.set_icon()
+        except:
+            pass
+
+    # Menu para agregar bases de datos
+        self.menu_base = tk.Menu(self)
+        self.menu_nuevo = tk.Menu(self.menu_base, tearoff=0)
+        self.menu_nuevo.add_command(
+            label='Agregar tabla certificados', command=self.cargar_certif)
+        self.menu_nuevo.add_command(
+            label='Agregar tabla empleados')#, command=self.Operador_nuevo)
+        self.menu_base.add_cascade(label='Agregar datos', menu=self.menu_nuevo)
+        self.config(menu=self.menu_base)
+
+        # Marco para los graficos
+        self.fr_graficos = tk.Frame(self, bg="green", height=400)
+        self.fr_graficos.pack(side="top", fill="x")
+        self.la_falta_tabla = tk.Label(
+            self.fr_graficos, text='Por favor cargue la tabla con' +
+            ' los certificados')
+
+        # Marco para los seleccionadores
+        self.fr_selec = tk.Frame(self, bg="red", height=400)
+        self.fr_selec.pack(side="bottom", fill="x")
+
+        # Marcos para los menus de seleccion
+        ''' 4 menus consecutivos (0 a 3) de izquierda a derecha.
+        El menu 2 solo aparece si es llamado del menu 1 y asi'''
+        self.fr_selec.columnconfigure((0, 1, 2, 3), weight=1)
+        self.fr_selec_0 = tk.Frame(self.fr_selec, bg="darkblue",
+                                   height=400)
+        self.fr_selec_1 = tk.Frame(self.fr_selec, bg="darkgreen", height=400)
+        self.fr_selec_2 = tk.Frame(self.fr_selec, bg="purple", height=400)
+        self.fr_selec_3 = tk.Frame(self.fr_selec, bg="violet", height=400)
+        self.fr_selec_0.grid(row=0, column=0, sticky="nsew")
+        self.fr_selec_1.grid(row=0, column=1, sticky="nsew")
+        self.fr_selec_2.grid(row=0, column=2, sticky="nsew")
+        self.fr_selec_3.grid(row=0, column=3, sticky="nsew")
+
+        # Frame 0 (izquierda)
+        tex_1 = 'Seleccione el gráfico que desea:'
+        self.lab_fr0 = tk.Label(self.fr_selec_0, text=tex_1)
+        self.lab_fr0.grid(row=0, column=0, sticky="w")
+
+        self.var_fr0 = tk.IntVar()
+        self.rb0_fr0 = tk.Radiobutton(self.fr_selec_0,
+                                      text='Ausencias totales',
+                                      variable=self.var_fr0, value=1,
+                                      command=self.distribuidor_frame_0)
+        self.rb1_fr0 = tk.Radiobutton(self.fr_selec_0,
+                                      text='Ausencias controlables' +
+                                      ' vs no controlables',
+                                      variable=self.var_fr0, value=2,
+                                      command=self.distribuidor_frame_0)
+        self.rb2_fr0 = tk.Radiobutton(self.fr_selec_0, text='Ausencias' +
+                                      ' justificadas vs injustificadas',
+                                      variable=self.var_fr0, value=3,
+                                      command=self.distribuidor_frame_0)
+        self.rb3_fr0 = tk.Radiobutton(self.fr_selec_0, text='Duración del' +
+                                      ' ausentismo',
+                                      variable=self.var_fr0, value=4,
+                                      command=self.distribuidor_frame_0)
+        self.rb0_fr0.grid(row=1, column=0, sticky="w")
+        self.rb1_fr0.grid(row=2, column=0, sticky="w")
+        self.rb2_fr0.grid(row=3, column=0, sticky="w")
+        self.rb3_fr0.grid(row=4, column=0, sticky="w")
+
+        # Frame 1 (izquierda centro)
+        # Frame interior (1b)
+        self.fr_selec_1b = tk.Frame(self.fr_selec_1)
+        tex_2 = 'Seleccione la frecuencia:'
+        self.lab_fr1 = tk.Label(self.fr_selec_1b, text=tex_2)
+        self.lab_fr1.grid(row=0, column=0, sticky="w")
+
+        self.var_fr1 = tk.IntVar()
+        self.rb0_fr1 = tk.Radiobutton(self.fr_selec_1b,
+                                      text='Por día',
+                                      variable=self.var_fr1, value=1,
+                                      command=self.distribuidor_frame_1)
+        self.rb1_fr1 = tk.Radiobutton(self.fr_selec_1b,
+                                      text='Por mes',
+                                      variable=self.var_fr1, value=2,
+                                      command=self.distribuidor_frame_1)
+        self.rb2_fr1 = tk.Radiobutton(self.fr_selec_1b, text='Por estación',
+                                      variable=self.var_fr1, value=3,
+                                      command=self.distribuidor_frame_1)
+        self.rb3_fr1 = tk.Radiobutton(self.fr_selec_1b, text='Por año',
+                                      variable=self.var_fr1, value=4,
+                                      command=self.distribuidor_frame_1)
+        self.rb0_fr1.grid(row=1, column=0, sticky="w")
+        self.rb1_fr1.grid(row=2, column=0, sticky="w")
+        self.rb2_fr1.grid(row=3, column=0, sticky="w")
+        self.rb3_fr1.grid(row=4, column=0, sticky="w")
+
+    # Funciones
+    def set_icon(self):
+        # Cargar el icono de la parte superior izquierda 
+        self.icon_image = tk.PhotoImage(file=logo)
+        self.iconphoto(False, self.icon_image)
+
+    def opciones_frecuencia(self, mostrar):
+        if mostrar == 1:
+            self.fr_selec_1b.pack()
+        else:
+            self.fr_selec_1b.forget()
+
+    def distribuidor_frame_0(self):
+        '''Asigna la fucnion correcta segun la eleccion en el frame de la
+        izquierda'''
+        if self.var_fr0.get() == 1:
+            self.opciones_frecuencia(1)
+        elif self.var_fr0.get() == 2:
+            self.opciones_frecuencia(1)
+        elif self.var_fr0.get() == 3:
+            self.opciones_frecuencia(1)
+        elif self.var_fr0.get() == 4:
+            self.opciones_frecuencia(0)
+
+    def distribuidor_frame_1(self):
+        '''Asigna la fucnion correcta segun la eleccion en el frame de la
+        izquierda'''
+        if not hasattr(self, 'tabla_SJ') or self.tabla_SJ.empty:
+            self.la_falta_tabla.forget()
+            self.la_falta_tabla.pack()
+        else:
+            self.la_falta_tabla.forget()
+            self.graf_1 = self.mostrar_grafico(self.var_fr1.get())
+
+    def cargar_certif(self):
+        '''Abre un menu para buscar en los archivos la tabla que se desea
+        cargar. Carga la tabla en memoria como data frame de pandas'''
+        opciones = {
+            'title': 'Seleccionar archivo',
+            'filetypes': [('Achivos compatibles', '*.xlsx')]
+        }
+        ruta_tabla = fd.askopenfilename(**opciones)
+        self.focus()
+        self.tabla_SJ1 = pd.read_excel(ruta_tabla)
+        self.limpiar_base()
+        self.la_falta_tabla.forget()
+
+    def limpiar_base(self):
+        # Eliminar la columna de fecha de fin de certificado
+        self.tabla_SJ2 = self.tabla_SJ1.drop('Validez_Hasta', axis=1)
+
+        # Eliminar filas con NaN en nro legajo o en fecha
+        self.tabla_SJ = self.tabla_SJ2.dropna(
+            subset=[self.tabla_SJ2.columns[3], self.tabla_SJ2.columns[9]])
+
+        # Eliminar filas con dias negativos
+        self.tabla_SJ = self.tabla_SJ[self.tabla_SJ["Dias"] > 0]
+
+    def crear_graf_total(self, frec=0):
+        lista_fechas = []
+        # Iterar sobre las filas del DataFrame
+        for _, row in self.tabla_SJ.iterrows():
+            # Generar un rango de fechas
+            fechas_evento = pd.date_range(
+                start=row['Validez_Desde'], periods=row['Dias']).tolist()
+            # Añadir la lista de fechas a la lista principal
+            lista_fechas.append(fechas_evento)
+        return lista_fechas
+
+    def mostrar_grafico(self, frec):
+        lista_fechas = self.crear_graf_total()
+        # Aplanar la lista de listas y contar las apariciones de cada fecha
+        fechas_planas = [fecha for sublista in
+                         lista_fechas for fecha in sublista]
+        conteo_fechas = Counter(fechas_planas)
+
+        # Crear DataFrame para ordenar y graficar
+        df_conteo_fechas = pd.DataFrame(conteo_fechas.items(),
+                                        columns=['Fecha', 'Frecuencia'])
+        df_conteo_fechas = df_conteo_fechas.sort_values('Fecha')
+
+        # Crear figura de Matplotlib
+        fig, ax = plt.subplots(figsize=(8, 4))
+        ax.plot(df_conteo_fechas['Fecha'], df_conteo_fechas['Frecuencia'],
+                marker='o', linestyle='-', color='skyblue')
+        ax.set_xlabel('Fecha')
+        ax.set_ylabel('Cantidad de ausencias')
+        ax.set_title('Ausencias por fecha')
+        plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        # Personalizar las etiquetas del eje x
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        # Limpiar el frame y mostrar el gráfico
+        for widget in self.fr_graficos.winfo_children():
+            widget.destroy()
+   
+        # Convertir figura a un Canvas de Tkinter y empaquetarlo en el frame
+        canvas = FigureCanvasTkAgg(fig, master=self.fr_graficos)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill="both", expand=True)
+
+# %% Ventana contenedora del grafico
+
+
+class VentanaGrafico(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %% Mostrar ventana principal
+
+
+if __name__ == '__main__':
+    VP = VentanaPrincipal()
+    VP.geometry("800x100")
+    VP.mainloop()
+
---
+++ Limpieza_base_SJ.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+# Imports
+import os
+import pandas as pd
+import sqlite3
+
+# %% Apertura de la base
+
+'''
+# Lenovo
+os.chdir("C:\\Users\\pablo\\OneDrive\\Tecnicatura Cs datos e IA\\"
+         "2do cuatri\\Practica I\\Practica San Jorge\\Datos San Jorge")
+'''
+# Acer
+
+os.chdir("C:\\Users\\Usuario\\OneDrive\\Tecnicatura Cs datos e IA\\"
+         "2do cuatri\\Practica I\\Práctica San Jorge\\Python San Jorge")
+
+base_original = 'ausentismo 22 a hoy.xlsx'
+
+# %% Separacion de datos para convertir base a SQL
+# Lectura de archivo excel
+base_df = pd.read_excel(base_original)
+
+# %% Creacion de base de datos SQL
+
+
+def crear_base():
+    conn = sqlite3.connect('Ausencias_SJ.db')
+    
+    # Habilitar claves foráneas
+    conn.execute("PRAGMA foreign_keys = ON")
+
+    cur = conn.cursor()
+
+    # Crear tabla de agrupadores
+    cur.execute('''CREATE TABLE IF NOT EXISTS agrupadores
+                (id_agr        INTEGER PRIMARY KEY,
+                 agrupador     TEXT            NOT NULL
+              );''')
+
+    # Crear tabla de valores departamentos
+    cur.execute('''CREATE TABLE IF NOT EXISTS departamentos
+                (id_dep        INTEGER PRIMARY KEY,
+                 DEPARTAMENTO  TEXT            NOT NULL
+              );''')
+
+    # Crear tabla de tipos de certificado
+    cur.execute('''CREATE TABLE IF NOT EXISTS tcd
+                (id_tc         INTEGER PRIMARY KEY,
+                 tc_detalle    TEXT,
+                 id_agr        INT,
+                 FOREIGN KEY (id_agr) REFERENCES agrupadores(id_agr)
+                 ON DELETE CASCADE
+              );''')
+
+    # Crear tabla de empleados
+    cur.execute('''CREATE TABLE IF NOT EXISTS empleados
+                (nro_legajo    INTEGER PRIMARY KEY,
+                 nombre        TEXT            NOT NULL,
+                 id_dep        INT               NOT NULL,
+                 FOREIGN KEY (id_DEP) REFERENCES departamentos(id_dep)
+                 ON DELETE CASCADE
+              );''')
+
+    # Crear tabla de certificados
+    cur.execute('''CREATE TABLE IF NOT EXISTS certificados
+                 (nro_certificado    INTEGER PRIMARY KEY,
+                  id_tc              INT             NOT NULL,
+                  nro_legajo         INT             NOT NULL,
+                  validez_dde        DATE            NOT NULL,
+                  dias               INT             NOT NULL,
+                  descripcion        TEXT,
+                  obs_general        TEXT
+                );''')
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+crear_base()
+
+
+def abrir_bd():
+    global conn
+    global cur
+    conn = sqlite3.connect('Ausencias_SJ.db')
+    cur = conn.cursor()
+
+
+def cerrar_bd():
+    conn.commit()
+    cur.close()
+    conn.close()
+
+# %% Verificaciones de datos de la base
+'''
+# Agrupa por el nombre y cuenta la cantidad de legajos únicos
+inconsistencias = base_df.groupby('Nombre')['Numero_Legajo'].nunique()
+inconsistencias_2 = base_df.groupby('Numero_Legajo')['Nombre'].nunique()
+# Muestra las filas donde hay más de un legajo para el mismo nombre
+inconsistencias_con_legajo_multiple = inconsistencias[inconsistencias > 1]
+inconsistencias_c_leg_multiple_2 = inconsistencias_2[inconsistencias_2 > 1]
+# Muestra los nombres que tienen más de un número de legajo
+print(inconsistencias_con_legajo_multiple)
+# Muestra los legajos que tienen más de un nombre
+print(inconsistencias_c_leg_multiple_2)
+
+# Verificar si hay números de certificado duplicados
+duplicados_certificado = base_df[base_df[
+    'Numero_Certificado'].duplicated(keep=False)]
+# Muestra los registros donde los números de legajo están repetidos
+print(duplicados_certificado)
+'''
+
+# %% Limpieza de la base
+
+# Convertir las fechas al formato correcto
+base_df['Validez_Desde'] = pd.to_datetime(base_df['Validez_Desde'],
+                                          dayfirst=True, errors='coerce')
+
+# Eliminar la columna de fecha de fin de certificado
+base_df_2 = base_df.drop('Validez_Hasta', axis=1)
+
+# Eliminar filas con NaN en nro legajo o en fecha
+base_df_limpia = base_df_2.dropna(
+    subset=[base_df_2.columns[3], base_df_2.columns[9]])
+
+# Convertir la columna "Validez_Desde" a un formato correcto para SQL
+base_df_limpia['Validez_desde'] = pd.to_datetime(
+    base_df_limpia['Validez_Desde']).dt.date
+
+# %% Agregado de columnas de ID
+
+# Agregar ID_TC (ID para cada tipo de certificado).
+base_df_limpia['ID_TC'] = pd.factorize(base_df_limpia['TCDetalle'])[0]
+# Agregar ID_Dep (ID para cada departamento)
+base_df_limpia['ID_Dep'] = pd.factorize(base_df_limpia['Departamento'])[0]
+# Agregar ID_Agr (ID para cada tipo de agrupador)
+base_df_limpia['ID_Agr'] = pd.factorize(base_df_limpia['Agrupador'])[0]
+
+# %% Dividir la tabla en sub tablas para SQL
+
+# Tabla T_TCD (Detalle de tipo de certificado)
+T_TCD = base_df_limpia[['ID_TC', 'TCDetalle', 'ID_Agr']].drop_duplicates()
+# Tabla T_Dep (Departamentos)
+T_Dep = base_df_limpia[['ID_Dep', 'Departamento']].drop_duplicates()
+# Tabla T_Agr (Agrupadores)
+T_Agr = base_df_limpia[['ID_Agr', 'Agrupador']].drop_duplicates()
+# Tabla T_Emp (Empleados)
+T_Emp = base_df_limpia[['Numero_Legajo', 'Nombre', 'ID_Dep']].drop_duplicates()
+# Tabla T_Cer (Certificados, tabla central)
+T_Cer = base_df_limpia[['Numero_Certificado', 'ID_TC', 'Numero_Legajo',
+                        'Validez_desde', 'Dias', 'Descripcion',
+                        'Observacion_General']].drop_duplicates()
+
+listado_databases = ((T_TCD, 'TCD'), (T_Dep, 'departamentos'),
+                     (T_Agr, 'agrupadores'), (T_Emp, 'empleados'),
+                     (T_Cer, 'certificados'))
+# %% Cargar los sub data frames en SQLITE
+
+
+def cargar_df_en_db(dataf, tablaSQL):
+    # Crear placeholders para los valores
+    placeholders = ', '.join('?' * len(dataf.columns))
+    # Convertir los datos del DataFrame a una lista de tuplas
+    valores = dataf.to_records(index=False).tolist()
+
+    abrir_bd()
+    # Crear la consulta SQL para insertar los datos
+    query = f"INSERT INTO {tablaSQL} VALUES ({placeholders})"
+    # Insertar los datos en la tabla existente
+    conn.executemany(query, valores)
+    cerrar_bd()
+
+
+for tabla in listado_databases:
+    cargar_df_en_db(tabla[0], tabla[1])
+
+
+# %% Contar las faltas por dia de la semana
+
+'''
+# Función para contar los días de la semana
+def contar_dias_semana(fecha_inicio, duracion):
+    if pd.isna(fecha_inicio) or duracion <= 0:  # Verifica fechas no válidas o duraciones incorrectas
+        return pd.Series([0]*7)  # Retorna cero para cada día si hay un error
+    dias = pd.date_range(fecha_inicio, periods=duracion)
+    return dias.weekday.value_counts().reindex(range(7), fill_value=0)
+
+# Aplicar la función a cada fila y expandir los resultados en columnas
+dias_semana = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
+conteo_dias = df.apply(lambda row: contar_dias_semana(row['Validez_Desde'], row['Dias']), axis=1)
+
+# Ya que 'conteo_dias' es un DataFrame, lo concatenamos directamente con 'df'
+df_con_dias = pd.concat([df, conteo_dias.set_axis(dias_semana, axis=1)], axis=1)
+
+
+df_con_dias.to_csv('ausencias_por_día.csv', index=False, sep = ';')
+'''
